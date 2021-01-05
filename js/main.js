83427199c16b913017acbe49deb18f0a3e2329d0
Alex <alexvanmanen@hotmail.com>
2021-01-05 15:54:06 +0100

    some code examples

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// const Person = Backbone.Model.extend({
+//     validate: function (attributes) {
+//         if(!attributes.surname) {
+//             return "surname is required";
+//         }
+//     }
+// });
+// // let backbonePerson = new Person();
+// // backbonePerson.set("name", "Alex");
+// // backbonePerson.set("surname", "van Manen");
+// //
+// // console.log(backbonePerson.get("name"));
+// // console.log(backbonePerson.attributes);
+//
+// let simplePerson = {};
+// simplePerson.name="Alex";
+// simplePerson.surname="van Manen";
+//
+// let backbonePerson = new Person(simplePerson);
+// console.log(simplePerson);
+//
+// backbonePerson.clear();
+// console.log(backbonePerson);
+// console.log(backbonePerson.isValid());
+
+var Todo = Backbone.Model.extend({
+    urlRoot: "https://jsonplaceholder.typicode.com/todos"
+});
+//
+// var todo = new Todo({id: 1});
+// todo.fetch();
+//
+// todo.set("title", "andere titel");
+// todo.save();
+// console.log(todo);
+// todo.destroy();
+//
+//
+// var todo = new Todo({title:"nieuwe todo"});
+// todo.save();
+
+var Todos = Backbone.Collection.extend({
+    url: "https://jsonplaceholder.typicode.com/todos",
+    model: Todo
+});
+
+var todos = new Todos();
+todos.fetch({
+    success: function () {
+        todos.forEach(function (todo) {
+            console.log(todo.get("title"));
+        })
+    },
+    error: () => console.log("ging mis")
+});
+
+todos.fetch().then(
+    mijnTodos => mijnTodos.forEach(todo =>console.log(todo.title)),
+    reason => console.log("ging fout, omdat " + reason.status)
+);
